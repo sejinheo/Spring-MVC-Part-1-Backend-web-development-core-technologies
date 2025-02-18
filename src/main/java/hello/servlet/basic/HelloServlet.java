950c77255a3909e3a373236f01aba9b0c84933bd
허세진 <heosejin222@gmail.com>
2025-02-18 12:03:52 +0900

    Hello 서블릿 설명 추가

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet(name="helloServlet", urlPatterns = "/hello")
+@WebServlet(name="helloServlet", urlPatterns = "/hello")//name-서블릿 이름, urlPatterns-URL 매핑
 public class HelloServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -19,8 +19,9 @@ public class HelloServlet extends HttpServlet {
         String username = request.getParameter("username");
         System.out.println("username = " + username);
 
-        response.setContentType("text/plain");
-        response.setCharacterEncoding("UTF-8");
-        response.getWriter().write("hello " + username);
+        response.setContentType("text/plain");//헤더 정보
+        response.setCharacterEncoding("UTF-8");//이제는 거의 다 utf-8로
+
+        response.getWriter().write("hello " + username);//body에 들어감
     }
 }
